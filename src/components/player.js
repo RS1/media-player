4f1458378c7147a6f1077b63a2abba6abb272ceb
RS1 Project <48027364+RS1-Project@users.noreply.github.com>
2021-01-15 20:34:32 +0000

    Fixed crossOrigin to use-credentials

--- src/components/player.js
+++ src/components/player.js
@@ -9,7 +9,7 @@
  * License: Apache License 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Modified on Friday, 15th January 2021 8:28:26 pm
+ * Modified on Friday, 15th January 2021 8:34:08 pm
  * *****************************************************************************
  */
 
@@ -445,7 +445,7 @@ export default ({ config, media: track, ...props }) => {
                     loop={options.loop}
                     autoPlay={options.autoPlay}
                     playsInline
-                    crossOrigin='anonymous'
+                    crossOrigin='use-credentials'
                     id='rs1-media-player-element'
                 />
                 {!metadata.video && options.vinylMode && (
